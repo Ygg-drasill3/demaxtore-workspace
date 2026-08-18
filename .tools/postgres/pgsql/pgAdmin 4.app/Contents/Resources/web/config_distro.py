91aeb6352cfc0ccc9cbeb459e11063597e7551a0
Ygg-drasill3 <Ygg-drasill3@users.noreply.github.com>
2026-08-18 12:30:53 +0000

    Add .data backups and .tools binaries via Git LFS.
    Store files over GitHub's 100MB git limit in LFS so upload backups and local Postgres/Node binaries can be versioned.

---
+++ .tools/postgres/pgsql/pgAdmin 4.app/Contents/Resources/web/config_distro.py
@@ -0,0 +1,8 @@
+SERVER_MODE = False
+HELP_PATH = '../../../docs/en_US/html/'
+DEFAULT_BINARY_PATHS = {
+    'pg':   '$DIR/../../SharedSupport',
+    'ppas': ''
+}
+MINIFY_HTML = False
+UPGRADE_CHECK_KEY = 'edb-pgadmin4'
